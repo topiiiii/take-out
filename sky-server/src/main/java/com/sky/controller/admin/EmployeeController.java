1e683464216cd41f20d0b517d82cf6e51b4f03ef
刘鑫 <12008652+linxin_11@user.noreply.gitee.com>
2024-04-11 00:47:27 +0800

    启用、禁用员工状态业务bug修改

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -103,9 +103,9 @@ public class EmployeeController {
 
     @PostMapping("/status/{status}")
     @ApiOperation("启用、禁用员工账号")
-    public Result startOrStop(@PathVariable Integer status,Long Id){
-        log.info("启用禁用员工账号:{},{}",status,Id);
-        employeeService.startOrStop(status,Id);
+    public Result startOrStop(@PathVariable Integer status,Long id){
+        log.info("启用禁用员工账号:{},{}",status,id);
+        employeeService.startOrStop(status,id);
         return Result.success();
     }
 }
